e8e711cc5563661430479b8eeb56731b2940784f
António Faneca <me@afaneca.com>
2020-02-22 23:11:27 +0000

    Preps for deployment (.env file)

--- api/public/libs/ensodb/config.php
+++ api/public/libs/ensodb/config.php
@@ -1,11 +1,16 @@
 <?php
 
-/* Database Settings */
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../' . '/../' . '/../' . '/../');
+$dotenv->load();
 
+$DB_HOST = getenv('DB_HOST');
+
+
+/* Database Settings */
 $databaseConfig["database_type"] = "mysql";
 $databaseConfig["server"] = "localhost";
-$databaseConfig["username"] = "admin";
-$databaseConfig["password"] = "kozoklaiu";
+$databaseConfig["username"] = getenv('DB_USER');
+$databaseConfig["password"] = getenv('DB_PW');
 $databaseConfig["charset"] = "utf8";
-$databaseConfig["port"] = "5432";
-$databaseConfig["database_name"] = "myfin";
+$databaseConfig["port"] = getenv('DB_PORT');
+$databaseConfig["database_name"] = getenv('DB_TABLE');
