3b74282ad4886658573f333ae12aac32254a08d7
António Faneca <me@afaneca.com>
2020-05-03 15:28:03 +0100

    % initial draft of stats page

---
+++ web/js/stats.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var Stats = {
+    init: () => {
+        chartUtils.setupSimpleLineChart("chart_pie_patrimony_evolution", null, null, "asdfsa")
+    },
+}
+
+//# sourceURL=js/stats.js
--- web/js/utils/chartUtils.js
+++ web/js/utils/chartUtils.js
@@ -57,7 +57,7 @@ var chartUtils = {
                     '#d5d5d5',
                 ],
                 borderWidth: 1,
-                
+
             }],
             labels: customLabels,
         };
@@ -113,6 +113,60 @@ var chartUtils = {
         var myPieChart = new Chart(ctx, {
             type: 'pie',
             data: chartData,
+            options: customOptions
+        });
+    },
+    setupSimpleLineChart: (elementID, chartData, chartLabels, chartTitle) => {
+        var ctx = document.getElementById(elementID).getContext('2d');
+
+        var customOptions = {
+            title: {
+                display: true,
+                text: chartTitle,
+                position: "top"
+            }
+        }
+
+        var data = {
+            labels: chartLabels,
+            /* datasets: [{
+                data: [0, 10, 20, 30, 100, 40, 56, 60, 70, 91, 300],
+                backgroundColor: chartUtils.getPieChartColorsList()
+
+            }] */
+            datasets: [{
+                data: [86, 114, 106, 106, 107, 111, 133, 221, 783, 2478],
+                label: "Africa",
+                borderColor: "#3e95cd",
+                fill: false
+            }, {
+                data: [282, 350, 411, 502, 635, 809, 947, 1402, 3700, 5267],
+                label: "Asia",
+                borderColor: "#8e5ea2",
+                fill: false
+            }, {
+                data: [168, 170, 178, 190, 203, 276, 408, 547, 675, 734],
+                label: "Europe",
+                borderColor: "#3cba9f",
+                fill: false
+            }, {
+                data: [40, 20, 10, 16, 24, 38, 74, 167, 508, 784],
+                label: "Latin America",
+                borderColor: "#e8c3b9",
+                fill: false
+            }, {
+                data: [6, 3, 2, 2, 7, 26, 82, 172, 312, 433],
+                label: "North America",
+                borderColor: "#c45850",
+                fill: false
+            }
+            ]
+        };
+
+
+        var myLineChart = new Chart(ctx, {
+            type: 'line',
+            data: data,
             options: customOptions
         });
     },
