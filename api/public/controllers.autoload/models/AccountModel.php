7eba6ae3b0878f12ca5001446865b2d7585b694c
António Faneca <me@afaneca.com>
2020-07-21 09:46:37 +0100

    * fixed issue with balance recalculation on transfer transactions

--- api/public/controllers.autoload/models/AccountModel.php
+++ api/public/controllers.autoload/models/AccountModel.php
@@ -328,7 +328,8 @@ class AccountModel extends Entity
             $trxAmount = Input::convertFloatToInteger($trx["amount"]);
 
             if ($trxType == DEFAULT_TYPE_EXPENSE_TAG
-                || ($trxType == DEFAULT_TYPE_TRANSFER_TAG && $trx["accounts_account_from_id"])) {
+                || ($trxType == DEFAULT_TYPE_TRANSFER_TAG && $trx["accounts_account_from_id"]
+                && $trx["accounts_account_from_id"] == $$accountID)) {
                 $trxAmount *= -1;
             }
             //print_r($trxList);
