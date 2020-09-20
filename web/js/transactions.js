0b12c1c197648eb7e642aeb5d7174127a70c3dcf
António Faneca <me@afaneca.com>
2020-09-20 19:35:23 +0100

    ! tweaked add/edit transaction forms

--- web/js/transactions.js
+++ web/js/transactions.js
@@ -112,13 +112,16 @@ var Transactions = {
                 
                     <form class="col s12">
                         <div class="row">
-                            <div class="input-field col s5">
-                                <i class="material-icons prefix">folder</i>
+                            <div class="input-field col s3">
+                                <i class="material-icons prefix">euro_symbol</i>
                                 <input id="trx_amount" type="number" step=".01" class="validate">
                                 <label for="trx_amount">Valor (€)</label>
                             </div>
-                            <input type="text" class="datepicker input-field col s5 offset-s1">
-                             
+                             <div class="input-field col s7">
+                                <i class="material-icons prefix">date_range</i>
+                                <input id="trx_date" type="text" class="datepicker input-field col s5 offset-s1">
+                                <label for="trx_date">Data da transação</label>
+                            </div>                             
                         </div>
                         <div class="row col s12">                     
                             <div class="input-field col s4">
@@ -337,13 +340,17 @@ var Transactions = {
                 
                     <form class="col s12">
                         <div class="row">
-                            <div class="input-field col s5">
-                                <i class="material-icons prefix">folder</i>
+                            <div class="input-field col s3">
+                                <i class="material-icons prefix">euro_symbol</i>
                                 <input id="trx_amount" type="number" step=".01" class="validate">
                                 <label for="trx_amount" class="active">Valor (€)</label>
                             </div>
-                            <input type="text" class="datepicker input-field col s5 offset-s1">
-                             
+                            
+                             <div class="input-field col s7">
+                                <i class="material-icons prefix">date_range</i>
+                                <input id="trx_date" type="text" class="datepicker input-field col s5 offset-s1">
+                                <label for="trx_date">Data da transação</label>
+                            </div>
                         </div>
                         <div class="row col s12">                     
                             <div class="input-field col s4">
