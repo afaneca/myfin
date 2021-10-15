4fe076a85a4902684845e5ceccf0b790626e0374
António Faneca <me@afaneca.com>
2021-10-16 00:43:36 +0100

    + empty view for charts in dashboard

---
+++ web/js/components/graphEmptyView.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const GraphEmptyViewComponent = {
+  buildDefaultGraphEmptyView: (label = 'Sem dados para apresentar') => `
+    <div style="margin: 0 auto;width: auto;text-align: center;">
+        <p>${label}</p>
+    </div>
+  `,
+};
+
+//# sourceURL=js/components/graphEmptyView.js
