e22970b8a866957c4b6f84de3bcda1668c2dc067
António Faneca <me@afaneca.com>
2022-08-11 14:24:22 +0100

    [#152] add fk on delete set null option to categories and entities tables

--- api/public/db/migrations/20220811130902_add_fk_on_delete_options_to_categories_and_entities.php
+++ api/public/db/migrations/20220811130902_add_fk_on_delete_options_to_categories_and_entities.php
@@ -7,14 +7,15 @@ class AddFkOnDeleteOptionsToCategoriesAndEntities extends Phinx\Migration\Abstra
     public function change()
     {
         $this->table('transactions', [
-                'id' => false,
-                'primary_key' => ['transaction_id'],
-                'engine' => 'InnoDB',
-                'encoding' => 'utf8',
-                'collation' => 'utf8_general_ci',
-                'comment' => '',
-                'row_format' => 'DYNAMIC',
-            ])
+            'id' => false,
+            'primary_key' => ['transaction_id'],
+            'engine' => 'InnoDB',
+            'encoding' => 'utf8',
+            'collation' => 'utf8_general_ci',
+            'comment' => '',
+            'row_format' => 'DYNAMIC',
+        ])->addForeignKey('entities_entity_id', 'entities', 'entity_id', array('delete' => 'set null', 'update' => 'no action'))
+            ->addForeignKey('categories_category_id', 'categories', 'category_id', array('delete' => 'set null', 'update' => 'no action'))
             ->save();
     }
 }
