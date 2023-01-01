180bd4d1182aca3fad8d4f2f90ebd2a83b1d8297
António Faneca <me@afaneca.com>
2023-01-01 17:49:55 +0000

    investments localization fixes

--- web/js/funcs/investmentDashboardChartsFunc.js
+++ web/js/funcs/investmentDashboardChartsFunc.js
@@ -5,10 +5,9 @@ import { Localization } from '../utils/localization.js'
 
 export const InvestmentDashboardChartsFunc = {
   buildInvestmentsvolutionLineChart: (canvasId, chartLabels, chartData, extraChartData) => {
-    var ctx = document.getElementById(canvasId)
-      .getContext('2d');
+    var ctx = document.getElementById(canvasId).getContext('2d')
 
-    const chartTitle = 'Evolução do Património';
+    const chartTitle = Localization.getString('investments.portfolioEvolution')
     var customOptions = {
       scales: {
         y: {
@@ -33,7 +32,7 @@ export const InvestmentDashboardChartsFunc = {
         },
         legend: {
           labels: {
-            color: LayoutUtils.getCSSVariableValue('--main-text-color')
+            color: LayoutUtils.getCSSVariableValue('--main-text-color'),
           },
           display: false,
         },
@@ -41,53 +40,52 @@ export const InvestmentDashboardChartsFunc = {
       tooltips: {
         callbacks: {
           title: function (tooltipItem, data) {
-            return data['labels'][tooltipItem[0]['index']];
+            return data['labels'][tooltipItem[0]['index']]
           },
           label: (tooltipItem, data) => {
-            return StringUtils.formatMoney(tooltipItem.value/*data['datasets'][0]['data'][tooltipItem['index']]*/);
+            return StringUtils.formatMoney(tooltipItem.value/*data['datasets'][0]['data'][tooltipItem['index']]*/)
           },
           afterLabel: (tooltipItem, data) => {
-          }
-        }
+          },
+        },
       },
-    };
+    }
 
     var data = {
       labels: chartLabels,
-      datasets: [{
-        data: chartData,
-        label: Localization.getString("common.accumulated"),
-        borderColor: '#3e95cd',
-        fill: true,
-        /*lineTension: 0,*/
-      },
-        ...extraChartData
-      ]
-    };
+      datasets: [
+        {
+          data: chartData,
+          label: Localization.getString('common.accumulated'),
+          borderColor: '#3e95cd',
+          fill: true,
+          /*lineTension: 0,*/
+        },
+        ...extraChartData,
+      ],
+    }
 
     var myLineChart = new Chart(ctx, {
       type: 'line',
       data: data,
-      options: customOptions
-    });
+      options: customOptions,
+    })
   },
   buildDashboardAssetsDistributionPieChartv2: (elementId, chartData) => {
-    $('#' + elementId)
-      .empty();
+    $('#' + elementId).empty()
     window.morrisObj = new Morris.Donut({
       element: elementId,
       data: chartData,
       colors: chartUtils.getPieChartColorsList(),
-      labelColor: LayoutUtils.getCurrentThemeName() === MYFIN.APP_THEMES.LIGHT ? "#2f2d2d" : "#ffffff",
+      labelColor: LayoutUtils.getCurrentThemeName() === MYFIN.APP_THEMES.LIGHT ? '#2f2d2d' : '#ffffff',
       resize: true,
       formatter: (y, data) => {
-        return parseFloat(y).toFixed(2) + '%';
-      }
-    });
+        return parseFloat(y).toFixed(2) + '%'
+      },
+    })
   },
   buildDashboardAssetsDistributionPieChart: (canvasId, chartLabels, chartData, chartTitle) => {
-    var ctx = document.getElementById(canvasId)
-      .getContext('2d');
+    var ctx = document.getElementById(canvasId).getContext('2d')
 
     var customOptions = {
       borderWidth: 0,
@@ -95,68 +93,71 @@ export const InvestmentDashboardChartsFunc = {
       title: {
         display: true,
         text: chartTitle,
-        position: 'top'
+        position: 'top',
       },
       legend: {
         labels: {
-          fontColor: LayoutUtils.getCSSVariableValue('--main-text-color')
+          fontColor: LayoutUtils.getCSSVariableValue('--main-text-color'),
         },
         display: false,
       },
       tooltips: {
         callbacks: {
           title: function (tooltipItem, data) {
-            return data['labels'][tooltipItem[0]['index']];
+            return data['labels'][tooltipItem[0]['index']]
           },
           label: (tooltipItem, data) => {
-            return StringUtils.formatMoney(data['datasets'][0]['data'][tooltipItem['index']]);
+            return StringUtils.formatMoney(data['datasets'][0]['data'][tooltipItem['index']])
           },
           afterLabel: (tooltipItem, data) => {
-            var dataset = data['datasets'][0];
+            var dataset = data['datasets'][0]
             //debugger
             //var metaData = dataset["_meta"].find(x => x !== undefined);
             var totalAmount = (() => {
               return dataset.data.reduce((acc, item) => {
-                return acc + Math.abs(parseFloat(item));
-              }, 0);
-            })();
+                return acc + Math.abs(parseFloat(item))
+              }, 0)
+            })()
             //debugger
-            var percent = Math.abs(Math.round((dataset['data'][tooltipItem['index']] / totalAmount) * 100));
-            return '(' + percent + '%)';
-          }
-        }
+            var percent = Math.abs(Math.round((dataset['data'][tooltipItem['index']] / totalAmount) * 100))
+            return '(' + percent + '%)'
+          },
+        },
       },
       scales: {
-        yAxes: [{
-          ticks: {
-            fontColor: LayoutUtils.getCSSVariableValue('--main-text-color'),
-          }
-        }],
-        xAxes: [{
-          ticks: {
-            fontColor: LayoutUtils.getCSSVariableValue('--main-text-color'),
-          }
-        }]
-      }
-    };
+        yAxes: [
+          {
+            ticks: {
+              fontColor: LayoutUtils.getCSSVariableValue('--main-text-color'),
+            },
+          }],
+        xAxes: [
+          {
+            ticks: {
+              fontColor: LayoutUtils.getCSSVariableValue('--main-text-color'),
+            },
+          }],
+      },
+    }
 
     var chartDataVar = {
       labels: chartLabels,
-      datasets: [{
-        data: chartData,
-        backgroundColor: chartUtils.getPieChartGradientsList(ctx)
+      datasets: [
+        {
+          data: chartData,
+          backgroundColor: chartUtils.getPieChartGradientsList(ctx),
 
-      }]
-    };
+        }],
+    }
 
     var myPieChart = new Chart(ctx, {
       type: 'doughnut',
       data: chartDataVar,
-      options: customOptions
-    });
+      options: customOptions,
+    })
 
-    return myPieChart;
-  }
-};
+    return myPieChart
+  },
+}
 
 //# sourceURL=js/funcs/investmentDashboardChartsFunc.js
