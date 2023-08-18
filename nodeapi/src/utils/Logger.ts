53e8b4356a9b836517648d06fd8b2ef1346a7c72
António Faneca <me@afaneca.com>
2023-08-18 11:06:01 +0100

    migrated Logger to typescript

--- nodeapi/src/utils/Logger.ts
+++ nodeapi/src/utils/Logger.ts
@@ -1,4 +1,4 @@
-const addLog = (logMessage, forceDisplay = false) => {
+const addLog = (logMessage: string, forceDisplay = false) => {
   const logsAreEnabled = process.env.LOGGING === 'true';
   // eslint-disable-next-line no-console
   if (logsAreEnabled || forceDisplay) {
@@ -6,7 +6,7 @@ const addLog = (logMessage, forceDisplay = false) => {
   }
 };
 
-const addStringifiedLog = (logMessage, forceDisplay = false) => {
+const addStringifiedLog = (logMessage: any, forceDisplay = false) => {
   addLog(JSON.stringify(logMessage), forceDisplay);
 };
 
