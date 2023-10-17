84b7c758ca02f627b508c4cd49e07a2fba251f25
António Faneca <me@afaneca.com>
2023-10-17 21:50:18 +0100

    [#339] Login: fix strings in sign in/sign up toggle

--- js/login.js
+++ js/login.js
@@ -1,234 +1,199 @@
-import {LocalDataManager} from './utils/localDataManager.js';
-import {LoadingManager} from './utils/loadingManager.js';
-import {Localization} from './utils/localization.js';
-import {DialogUtils} from './utils/dialogUtils.js';
+import { LocalDataManager } from './utils/localDataManager.js'
+import { LoadingManager } from './utils/loadingManager.js'
+import { Localization } from './utils/localization.js'
+import { DialogUtils } from './utils/dialogUtils.js'
 
-const SIGN_UP_HTML = Localization.getString('login.notYetRegisteredQuestion');
-const SIGN_IN_HTML = Localization.getString('login.alreadyRegisteredQuestion');
-const SIGN_UP_BTN_TXT = Localization.getString(
-    'login.alreadyRegisteredQuestion');
-const SIGN_IN_BTN_TXT = Localization.getString('login.signIn');
+var isSignUp = false
 
-var isSignUp = false;
-
-$('.login-form').submit(function(e) {
-  e.preventDefault();
+$('.login-form').submit(function (e) {
+  e.preventDefault()
   //configs.switchApp("myfin");
 
-  var email = $('#email-input').val();
-  var username = $('#login-input').val();
-  var pw = $('#pw-input').val();
+  var email = $('#email-input').val()
+  var username = $('#login-input').val()
+  var pw = $('#pw-input').val()
 
   if (isSignUp) {
-    addUser(username, email, pw);
+    addUser(username, email, pw)
   } else {
-    performLogin(username, pw);
+    performLogin(username, pw)
   }
-});
+})
 
-export function signUpLinkWasClicked() {
-  isSignUp = !isSignUp;
+export function signUpLinkWasClicked () {
+  debugger
+  isSignUp = !isSignUp
 
   if (isSignUp) {
-    showSignUp();
+    showSignUp()
   } else {
-    showSignIn();
+    showSignIn()
   }
 }
 
-export function showSignUp() {
-  const signUpSelector = $('#change-login-type');
-  const emailInputSelector = $('#email-input');
-  const btnSelector = $('#btn-login');
+export function showSignUp () {
+  const signUpSelector = $('#change-login-type')
+  const emailInputSelector = $('#email-input')
+  const btnSelector = $('#btn-login')
 
-  btnSelector.html(SIGN_UP_BTN_TXT);
-  signUpSelector.html(SIGN_UP_HTML);
-  //emailInputSelector.show()
-  emailInputSelector.removeClass('input_hidden');
-  emailInputSelector.addClass('input_shown');
-
+  emailInputSelector.removeClass('input_hidden')
+  emailInputSelector.addClass('input_shown')
+  btnSelector.html(Localization.getString(
+    'login.signUp'))
+  signUpSelector.html(Localization.getString('login.alreadyRegisteredQuestion'))
 }
 
-export function showSignIn() {
-  const signInSelector = $('#change-login-type');
-  const emailInputSelector = $('#email-input');
-  const btnSelector = $('#btn-login');
+export function showSignIn () {
+  const signInSelector = $('#change-login-type')
+  const emailInputSelector = $('#email-input')
+  const btnSelector = $('#btn-login')
 
-  btnSelector.html(SIGN_IN_BTN_TXT);
-  signInSelector.html(SIGN_IN_HTML);
-  //emailInputSelector.hide()
-  emailInputSelector.removeClass('input_shown');
-  emailInputSelector.addClass('input_hidden');
-
-  var username = $('#username-input').val();
-  var pw = $('#pw-input').val();
+  emailInputSelector.removeClass('input_shown')
+  emailInputSelector.addClass('input_hidden')
+  btnSelector.html(Localization.getString('login.signIn'))
+  signInSelector.html(Localization.getString('login.notYetRegisteredQuestion'))
 }
 
-export function checkCredentials() {
+export function checkCredentials () {
   if (browserHasGoodCookies()) {
-    validateSession();
+    validateSession()
 
   } else {
-    resetSession();
+    resetSession()
   }
 }
 
-async function validateSession(renewValidity = undefined) {
-  var loginInvalid = true;
-  var pageUrl = REST_SERVER_PATH + 'validity/';
-  const {configs} = await import('./configs.js');
+async function validateSession (renewValidity = undefined) {
+  var loginInvalid = true
+  var pageUrl = REST_SERVER_PATH + 'validity/'
+  const { configs } = await import('./configs.js')
   $.ajax({
-    async: true,
-    type: 'POST',
-    dataType: 'json',
-    cache: false,
-    headers: {
+    async: true, type: 'POST', dataType: 'json', cache: false, headers: {
       authusername: Cookies.get('username'),
       sessionkey: Cookies.get('sessionkey'),
-    },
-    data: {
+    }, data: {
       renewValidity: renewValidity,
-    },
-    url: pageUrl,
-    success: function(response) {
+    }, url: pageUrl, success: function (response) {
       if (response == '1') {
-        loginInvalid = false;
+        loginInvalid = false
       }
       // there's a valid sessionkey, skip the login stages (if we're in login
       // page)
       if (window.location.pathname.split('/').pop() == 'login.html') {
-        configs.switchApp('myfin');
+        configs.switchApp('myfin')
       }
-    },
-    error: function(response) {
+    }, error: function (response) {
       if (window.location.pathname.split('/').pop() != 'login.html') // If we are not
-          // already
-          // on the
-          // login
-          // page
+        // already
+        // on the
+        // login
+        // page
       {
-        configs.switchApp('login');
+        configs.switchApp('login')
       }
     },
-  });
+  })
 
-  return !loginInvalid;
+  return !loginInvalid
 }
 
-export function checkPreAuthWithInterval(interval = 300000) { // 300000 ms = 5 mins
+export function checkPreAuthWithInterval (interval = 300000) { // 300000 ms = 5 mins
   setInterval(() => {
-    checkPreAuth(undefined, false);
-  }, interval);
+    checkPreAuth(undefined, false)
+  }, interval)
 }
 
-export async function checkPreAuth(
-    failFunction = undefined, renewValidity = undefined) {
+export async function checkPreAuth (
+  failFunction = undefined, renewValidity = undefined) {
 
   if (Cookies.get('username') && Cookies.get('sessionkey')) // If there are
-      // records of
-      // username &
-      // sessionkey in
-      // Cookies
+    // records of
+    // username &
+    // sessionkey in
+    // Cookies
   {
-    await validateSession(renewValidity);
+    await validateSession(renewValidity)
   }// Check if those credentials are still valid
 
   else {
     //console.log("Não presente nas Cookies. Pedir Login.");
     if (failFunction) {
-      failFunction;
+      failFunction
     }
     if (window.location.pathname.split('/').pop() != 'login.html') // If we are not
-        // already on
-        // the login
-        // page
+      // already on
+      // the login
+      // page
     {
-      const {configs} = await import('./configs.js').then((c) => {
-        window.location = c.defaultApp + '.html';
-      });
+      const { configs } = await import('./configs.js').then((c) => {
+        window.location = c.defaultApp + '.html'
+      })
 
     } // Redirect to login page
   }
 }
 
-function addUser(username, email, password) {
+function addUser (username, email, password) {
   if (!email || !username || !password) {
     DialogUtils.showSuccessMessage(
-        Localization.getString('login.fillAllFields'));
-    return;
+      Localization.getString('login.fillAllFields'))
+    return
   }
-  disableLoginBtn();
+  disableLoginBtn()
   showLoading().then(r => {
-    var pageUrl = REST_SERVER_PATH + 'users/';
+    var pageUrl = REST_SERVER_PATH + 'users/'
     $.ajax({
-      async: true,
-      type: 'POST',
-      dataType: 'json',
-      cache: false,
-      headers: {
+      async: true, type: 'POST', dataType: 'json', cache: false, headers: {
         authusername: Cookies.get('username'),
         sessionkey: Cookies.get('sessionkey'),
-      },
-      data: {
-        username,
-        email,
-        password,
-      },
-      url: pageUrl,
-      success: (response) => {
+      }, data: {
+        username, email, password,
+      }, url: pageUrl, success: (response) => {
         hideLoading().then(r => {
           DialogUtils.showSuccessMessage(
-              Localization.getString('login.userSuccessfullyAdded'));
-          signUpLinkWasClicked();
-          enableLoginBtn();
-        });
-      },
-      error: (response) => {
+            Localization.getString('login.userSuccessfullyAdded'))
+          signUpLinkWasClicked()
+          enableLoginBtn()
+        })
+      }, error: (response) => {
         hideLoading().then(r => {
           DialogUtils.showErrorMessage(
-              response.status === 401 ? Localization.getString(
-                  'login.addUserDisabledError') : undefined);
-          enableLoginBtn();
-        });
+            response.status === 401 ? Localization.getString(
+              'login.addUserDisabledError') : undefined)
+          enableLoginBtn()
+        })
       },
 
-    });
-  });
+    })
+  })
 }
 
-function disableLoginBtn() {
-  $('#btn-login').prop('disabled', true);
+function disableLoginBtn () {
+  $('#btn-login').prop('disabled', true)
 }
 
-function enableLoginBtn() {
-  $('#btn-login').prop('disabled', false);
+function enableLoginBtn () {
+  $('#btn-login').prop('disabled', false)
 }
 
-async function performLogin(username, password) {
-  var pageUrl = REST_SERVER_PATH + 'auth/';
-  const {configs} = await import('./configs.js');
-  disableLoginBtn();
+async function performLogin (username, password) {
+  var pageUrl = REST_SERVER_PATH + 'auth/'
+  const { configs } = await import('./configs.js')
+  disableLoginBtn()
   showLoading().then(r => {
     $.ajax({
-      async: true,
-      type: 'POST',
-      dataType: 'json',
-      cache: false,
-      data: {
-        username: $('#login-input').val(),
-        password: $('#pw-input').val(),
-      },
-      url: pageUrl,
-      success: function(response) {
+      async: true, type: 'POST', dataType: 'json', cache: false, data: {
+        username: $('#login-input').val(), password: $('#pw-input').val(),
+      }, url: pageUrl, success: function (response) {
         //console.log(response);
-        enableLoginBtn();
+        enableLoginBtn()
         hideLoading().then(r => {
-          Cookies.set('sessionkey', response['sessionkey']);
-          Cookies.set('username', response['username']);
-          Cookies.set('trustlimit', response['trustlimit']);
-          LocalDataManager.setUserAccounts(response['accounts']);
+          Cookies.set('sessionkey', response['sessionkey'])
+          Cookies.set('username', response['username'])
+          Cookies.set('trustlimit', response['trustlimit'])
+          LocalDataManager.setUserAccounts(response['accounts'])
 
-          configs.switchApp('myfin');
+          configs.switchApp('myfin')
           /* if (!misc.checkIfUserHasAction("canAccessBackoffice")) {
             M.toast({
               html: "Não tem permissões para aceder ao Backoffice!"
@@ -238,54 +203,50 @@ async function performLogin(username, password) {
             Cookies.set("username", response["username"]);
             ensoConf.switchApp("enso_life");
           } */
-        });
-      },
-      error: function(response) {
-        enableLoginBtn();
+        })
+      }, error: function (response) {
+        enableLoginBtn()
         hideLoading().then(r => {
           if (response.status == EnsoShared.ENSO_REST_NOT_AUTHORIZED) {
             /* M.toast('Autenticação falhada.', 3000, 'rounded'); */
           }
 
           DialogUtils.showErrorMessage(
-              Localization.getString('login.wrongCredentialsError'));
-        });
+            Localization.getString('login.wrongCredentialsError'))
+        })
       },
-    });
-  });
+    })
+  })
 }
 
-async function showLoading() {
-  LoadingManager.showLoading();
+async function showLoading () {
+  LoadingManager.showLoading()
 }
 
-async function hideLoading() {
-  LoadingManager.hideLoading();
+async function hideLoading () {
+  LoadingManager.hideLoading()
 }
 
 /* Removes all user data from Cookies */
-export async function resetSession() {
-  Cookies.remove('sessionkey');
-  Cookies.remove('username');
-  Cookies.remove('trustlimit');
+export async function resetSession () {
+  Cookies.remove('sessionkey')
+  Cookies.remove('username')
+  Cookies.remove('trustlimit')
 
-  LocalDataManager.clearLocalSessionData();
+  LocalDataManager.clearLocalSessionData()
 
   if (window.location.pathname.split('/').pop() != 'login.html') // if we're not
-      // in the login
-      // page
+    // in the login
+    // page
   {
-    const {configs} = await import('./configs.js');
+    const { configs } = await import('./configs.js')
 
-    window.location = configs.defaultApp + '.html';
+    window.location = configs.defaultApp + '.html'
   } // redirect to login page
 }
 
 /* Checks if the browser has stored all the user information required */
-function browserHasGoodCookies() {
-  return (
-      Cookies.get('sessionkey') !== undefined &&
-      Cookies.get('username') !== undefined &&
-      Cookies.get('trustlimit') !== undefined
-  );
+function browserHasGoodCookies () {
+  return (Cookies.get('sessionkey') !== undefined && Cookies.get('username') !==
+    undefined && Cookies.get('trustlimit') !== undefined)
 }
