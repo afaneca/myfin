48e9c28d88d3539411874cd80d1994d1429724e8
António Faneca <me@afaneca.com>
2024-06-10 23:55:14 +0100

    switched from BrowserRouter to HashRouter

--- react/src/providers/RoutesProvider.tsx
+++ react/src/providers/RoutesProvider.tsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Login from "../features/auth/Login.tsx";
-import PrivateRoute from "../components/PrivateRoute.tsx";
-import Dashboard from "../features/dashboard/Dashboard.tsx";
-import Profile from "../features/profile/Profile.tsx";
-import Transactions from "../features/transactions/Transactions.tsx";
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Login from '../features/auth/Login.tsx';
+import PrivateRoute from '../components/PrivateRoute.tsx';
+import Dashboard from '../features/dashboard/Dashboard.tsx';
+import Profile from '../features/profile/Profile.tsx';
+import Transactions from '../features/transactions/Transactions.tsx';
 
-export const ROUTE_AUTH = "/auth";
-export const ROUTE_DASHBOARD = "/dashboard";
-export const ROUTE_PROFILE = "/profile";
-export const ROUTE_TRX = "/transactions";
+export const ROUTE_AUTH = '/auth';
+export const ROUTE_DASHBOARD = '/dashboard';
+export const ROUTE_PROFILE = '/profile';
+export const ROUTE_TRX = '/transactions';
 
 const RoutesProvider = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path={`${ROUTE_AUTH}`} element={<Login />} />
         <Route element={<PrivateRoute />}>
@@ -23,7 +23,7 @@ const RoutesProvider = () => {
           <Route path="*" element={<Navigate to="/auth" replace={true} />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
